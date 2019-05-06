c9acb7b9656e93b6f743d4a62116cb037557b944
Angelika <Angelika>
2019-05-06 22:10:59 +0200

    added ArrayList for results

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,27 +1,56 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
+    private List results;
+
+    public Calculator() {
+        results = new ArrayList();
+    }
+
+    public List getResults() {
+        return results;
+    }
+
+    public double getLastResult() {
+        return (double)results.get(results.size()-1);
+    }
 
     public double add(double a, double b) {
-        return a + b;
+        double result = a + b;
+        results.add(result);
+        return result;
     }
 
     public double subtraction(double a, double b) {
-        return a - b;
+        double result = a - b;
+        results.add(result);
+        return result;
     }
 
     public double multiplication(double a, double b) {
-        return a * b;
+        double result = a * b;
+        results.add(result);
+        return result;
     }
 
     public double division(double a, double b) throws DivideByZeroException {
         if (b == 0) {
             throw new DivideByZeroException();
         }
-        return a / b;
+        double result = a / b;
+        results.add(result);
+        return result;
     }
 
     public boolean isEvenNumber(int a) {
         return a % 2 == 0;
+    }
+
+    public boolean isFloat(double a) {
+        int dec = (int)a;
+        return  (a - dec != 0);
     }
 }
