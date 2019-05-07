2e3e441b15c0956833916177a3cf7898464373b6
Angelika <Angelika>
2019-05-07 10:50:08 +0200

    updated CalculatorTest.java

--- src/com/company/test/CalculatorTest.java
+++ src/com/company/test/CalculatorTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -20,11 +23,27 @@ class CalculatorTest {
     }
 
     @Test
-    void fillResultList() {
+    void fillResultList() throws DivideByZeroException {
+        List results = new ArrayList();
+
+        results.add(calculator.add(2, 3));
+        results.add(calculator.subtraction(3,6));
+        results.add(calculator.division(98, 3));
+        results.add(calculator.multiplication(6, 3));
+
+        List calculatorResultList = calculator.getResults();
+
+        for (int i = 0; i < results.size(); i++) {
+            assertEquals(results.get(i), calculatorResultList.get(i));
+        }
     }
 
     @Test
     void getLastResult() {
+        calculator.add(2, 3);
+        calculator.subtraction(3,6);
+
+        assertEquals(-3, calculator.getLastResult());
     }
 
     @Test
@@ -47,8 +66,11 @@ class CalculatorTest {
         assertEquals(number * number2, result);
     }
 
-    @Test
-    void division() {
+    @ParameterizedTest
+    @CsvSource(value = {"2, 9", "283.6, 94"})
+    void division(double number, double number2) throws DivideByZeroException {
+        double result = calculator.division(number, number2);
+        assertEquals(number / number2, result);
     }
 
     @Test
@@ -66,7 +88,14 @@ class CalculatorTest {
         }
     }
 
-    @Test
-    void isFloat() {
+    @ParameterizedTest
+    @ValueSource(doubles = {3, 8, 45, 2, 98, 4})
+    void isFloat(double number) {
+        int dec = (int)number;
+        if (number - dec != 0) {
+            assertFalse(calculator.isFloat(number));
+        } else {
+            assertTrue(calculator.isFloat(number));
+        }
     }
 }
